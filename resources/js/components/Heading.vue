5e771e705c3357a5db383c4f84dec2d8e08e9822
Katelynn Barlowe <katelynn.barlowe@gmail.com>
2022-07-06 16:15:22 -0400

    abstraction

---
+++ resources/js/components/Heading.vue
@@ -0,0 +1,14 @@
+<template>
+    <Component :is="as" class="italic text-4xl font-serif">
+        <slot />
+    </Component>
+</template>
+<script>
+export default {
+    props: {
+        as: {
+            default: 'h1'
+        }
+    }
+}
+</script>
